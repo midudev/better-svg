4933456136234ef6e8d3c1c94491115247052736
Miguel Ángel Durán (midudev) <miduga@gmail.com>
2026-06-15 14:57:17 +0200

    chore: migrate linting to eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,39 @@
-import neostandard from "neostandard";
+const js = require('@eslint/js')
+const prettier = require('eslint-config-prettier')
+const globals = require('globals')
+const tseslint = require('typescript-eslint')
 
-export default neostandard({
-  ts: true,
-  ignores: ["dist/**", "node_modules/**", "src/webview/**"],
-});
+module.exports = tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**', 'src/webview/**']
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['src/**/*.{js,ts}'],
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs'
+    },
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off'
+    }
+  },
+  {
+    files: ['src/**/*.ts'],
+    rules: {
+      'no-undef': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' }
+      ],
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  },
+  prettier
+)
